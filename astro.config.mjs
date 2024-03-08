1000c178105536ab0e4038d23fab89e39cf362af
Alvaro Cebrian 2A <113922067+CebrianAlvaro9@users.noreply.github.com>
2024-03-08 12:08:38 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://test',
-  base: '/',
+  site: 'https://CebrianAlvaro9.github.io',
+  base: '/porfolioWebAlvaroCebrian',
 })
